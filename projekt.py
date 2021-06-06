57e5c95c0ee2a20e76624f3b0cae9f4fe1788262
Haoyang <sunhaoyang2138@gmail.com>
2021-06-06 14:29:08 +0200

    winfo project, data upload

--- projekt.py
+++ projekt.py
@@ -1,5 +1,6 @@
 import streamlit as st       
-import pandas as pd    
+import pandas as pd  
+import numpy as np  
 
 st.title("Daten Analyse")
 
@@ -9,3 +10,14 @@ eine kurze Beschreibung der Analyse
 
 dataset_name = st.selectbox("Select Dataset", ("Sollfahrtdaten", "Istfahrtdaten"))
 st.write(dataset_name)
+
+def get_dataset(dataset_name):
+    if dataset_name == "Sollfahrtdaten":
+        csv = pd.read_csv('592M_expected.csv')
+    else :
+        csv = pd.read_csv('592M_actual.csv')
+    return csv
+df = get_dataset(dataset_name)
+# df['Date'] = pd.to_datetime(df.date, infer_datetime_format = True)
+# df.sort_values(by='Date', ascending = True, inplace = True)
+st.write(df)
